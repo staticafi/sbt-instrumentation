783486d73fe697609e41a7af4f08f973b6542182
Martina Vitovska <409920@mail.muni.cz>
2017-02-22 13:25:06 +0100

    Rename fsm -> rec
    Signed-off-by: Martina Vitovska <409920@mail.muni.cz>

--- instrumentations/memsafety/memsafety.c
+++ instrumentations/memsafety/memsafety.c
@@ -2,64 +2,64 @@
 #include <stdint.h>
 #include <assert.h>
 
-typedef void* fsm_id;
+typedef void* rec_id;
 typedef uint64_t a_size;
 
 extern void __VERIFIER_error() __attribute__((noreturn));
 
 typedef enum {
-	FSM_STATE_ALLOCATED,
-	FSM_STATE_FREED,
-	FSM_STATE_NONE
-} fsm_state;
+	REC_STATE_ALLOCATED,
+	REC_STATE_FREED,
+	REC_STATE_NONE
+} rec_state;
 
-// finite state machine
+// record for a memory block
 typedef struct {
-	fsm_id id;
+	rec_id id;
 	a_size size;
-	fsm_state state;
-} fsm;
+	rec_state state;
+} rec;
 
 
-// FSM list type
-typedef struct fsm_list_node{
-	fsm *fsm;
-	struct fsm_list_node *next;
-} fsm_list_node;
+// list of records type
+typedef struct rec_list_node{
+	rec *rec;
+	struct rec_list_node *next;
+} rec_list_node;
 
-fsm_list_node *fsm_list = NULL;
+rec_list_node *rec_list = NULL;
 
-static void __INSTR_fsm_list_prepend(fsm_list_node *node) {
-	node->next = fsm_list;
-	fsm_list = node;
+static void __INSTR_rec_list_prepend(rec_list_node *node) {
+	node->next = rec_list;
+	rec_list = node;
 }
 
-fsm* __INSTR_fsm_create(fsm_id id, fsm_state state) {
-	fsm *new_fsm = (fsm *) malloc(sizeof(fsm));
-	new_fsm->id = id;
-	new_fsm->state = state;
+rec* __INSTR_rec_create(rec_id id, rec_state state) {
+	rec *new_rec = (rec *) malloc(sizeof(rec));
+	new_rec->id = id;
+	new_rec->state = state;
 
-	fsm_list_node *node = (fsm_list_node *) malloc(sizeof(fsm_list_node));
+	rec_list_node *node = (rec_list_node *) malloc(sizeof(rec_list_node));
 	node->next = NULL;
-	node->fsm = new_fsm;
+	node->rec = new_rec;
 
-	__INSTR_fsm_list_prepend(node);
+	__INSTR_rec_list_prepend(node);
 
-	return new_fsm;
+	return new_rec;
 }
 
-fsm* __INSTR_fsm_list_search(fsm_id id) {
-  	fsm_list_node *cur = fsm_list;
+rec* __INSTR_rec_list_search(rec_id id) {
+  	rec_list_node *cur = rec_list;
 
 	while(cur) {
 		/* check wether 'id' is a pointer to
 		 * some memory that we registred, that is if it points
 		 * to some memory region in range [cur.id, cur.id + size] */
-		if (cur->fsm->id <= id
-		     && (cur->fsm->id == id || // remove this part of condition
-		                               // once we know that fsm->size != 0
-		         (id - cur->fsm->id < cur->fsm->size))) {
-			return cur->fsm;
+		if (cur->rec->id <= id
+		     && (cur->rec->id == id || // remove this part of condition
+		                               // once we know that rec->size != 0
+		         (id - cur->rec->id < cur->rec->size))) {
+			return cur->rec;
 		}
 
 		cur = cur->next;
@@ -68,18 +68,17 @@ fsm* __INSTR_fsm_list_search(fsm_id id) {
 	return NULL;
 }
 
-// FSM manipulation
-void __INSTR_free(fsm_id id) {
+void __INSTR_free(rec_id id) {
 
-	// there is no FSM for NULL
+	// there is no record for NULL
 	if (id == 0) {
 		return;
 	}
 
-	fsm *m = __INSTR_fsm_list_search(id);
+	rec *m = __INSTR_rec_list_search(id);
 	
 	// Memory was already freed - double free error
-	if (m != NULL && m->state == FSM_STATE_FREED) {
+	if (m != NULL && m->state == REC_STATE_FREED) {
 	    assert(0 && "double free");
 		__VERIFIER_error();
 	}
@@ -88,49 +87,49 @@ void __INSTR_free(fsm_id id) {
 		assert(0 && "free on non-allocated memory");
 		__VERIFIER_error();
 	} else {
-		m->state = FSM_STATE_FREED;
+		m->state = REC_STATE_FREED;
 	}
 
 	
 }
 
-void __INSTR_remember(fsm_id id, a_size size, int num) {
+void __INSTR_remember(rec_id id, a_size size, int num) {
 	
-	fsm *m = __INSTR_fsm_list_search(id);
+	rec *m = __INSTR_rec_list_search(id);
 
 	if(m != NULL){
-		// If fsm already exists, change the size. This happens because
+		// If rec already exists, change the size. This happens because
 		// automatons created by alloca instructions are not destroyed at
 		// return of the function as they shoud be. This is just a temporary
 		// solution. 
-		m->state = FSM_STATE_NONE;
+		m->state = REC_STATE_NONE;
 		m->size = size*num;
 		return;
 	}
  
-	fsm *new_rec = (fsm *) malloc(sizeof(fsm));
+	rec *new_rec = (rec *) malloc(sizeof(rec));
 	new_rec->id = id;
 	new_rec->size = size * num;
-        new_rec->state = FSM_STATE_NONE;
+        new_rec->state = REC_STATE_NONE;
 
-	fsm_list_node *node = (fsm_list_node *) malloc(sizeof(fsm_list_node));
+	rec_list_node *node = (rec_list_node *) malloc(sizeof(rec_list_node));
 	node->next = NULL;
-	node->fsm = new_rec;
+	node->rec = new_rec;
 
-	__INSTR_fsm_list_prepend(node);
+	__INSTR_rec_list_prepend(node);
 }
 
-void __INSTR_remember_malloc_calloc(fsm_id id, size_t size, int num ) {
-	// there is no FSM for NULL
+void __INSTR_remember_malloc_calloc(rec_id id, size_t size, int num ) {
+	// there is no record for NULL
 	if (id == 0) {
 		return;
 	}
 
-	fsm *m = __INSTR_fsm_list_search(id);
+	rec *m = __INSTR_rec_list_search(id);
 	if (m != NULL) {
-		m->state = FSM_STATE_ALLOCATED;
+		m->state = REC_STATE_ALLOCATED;
 	} else {
-		m = __INSTR_fsm_create(id, FSM_STATE_ALLOCATED);
+		m = __INSTR_rec_create(id, REC_STATE_ALLOCATED);
 	}
 
 	if (m != NULL) {
@@ -138,9 +137,9 @@ void __INSTR_remember_malloc_calloc(fsm_id id, size_t size, int num ) {
 	}
 }
 
-void __INSTR_check_str_length(fsm_id dest, fsm_id source) {
-	fsm *rd = __INSTR_fsm_list_search(dest);
-	fsm *rs = __INSTR_fsm_list_search(source);
+void __INSTR_check_str_length(rec_id dest, rec_id source) {
+	rec *rd = __INSTR_rec_list_search(dest);
+	rec *rs = __INSTR_rec_list_search(source);
 
 	if (rd != NULL && rs != NULL) {
 		if (rd->size < rs->size) {
@@ -149,7 +148,7 @@ void __INSTR_check_str_length(fsm_id dest, fsm_id source) {
 		}
 
 		// this memory was already freed
-		if(rs->state == FSM_STATE_FREED || rd->state == FSM_STATE_FREED) {
+		if(rs->state == REC_STATE_FREED || rd->state == REC_STATE_FREED) {
 			assert(0 && "strcpy on freed memory");
 			__VERIFIER_error();
 		}
@@ -162,8 +161,8 @@ void __INSTR_check_str_length(fsm_id dest, fsm_id source) {
 	}
 }
 
-void __INSTR_check_pointer(fsm_id id, a_size range) {
-	fsm *r = __INSTR_fsm_list_search(id);
+void __INSTR_check_pointer(rec_id id, a_size range) {
+	rec *r = __INSTR_rec_list_search(id);
 
 	if (r != NULL) {
 		if (range > r->size ||
@@ -176,7 +175,7 @@ void __INSTR_check_pointer(fsm_id id, a_size range) {
 		}
 
 		// this memory was already freed
-		if(r->state == FSM_STATE_FREED) {
+		if(r->state == REC_STATE_FREED) {
 			assert(0 && "dereference on freed memory");
 			__VERIFIER_error();
 		}
@@ -189,21 +188,21 @@ void __INSTR_check_pointer(fsm_id id, a_size range) {
 	}
 }
 
-void __INSTR_fsm_destroy(fsm_id id) {
-    fsm_list_node *cur = fsm_list;
+void __INSTR_rec_destroy(rec_id id) {
+    rec_list_node *cur = rec_list;
 
-    if(cur && cur->fsm->id == id) {
-        fsm_list_node *newHead = cur->next;
-        free(cur->fsm);
+    if(cur && cur->rec->id == id) {
+        rec_list_node *new_head = cur->next;
+        free(cur->rec);
         free(cur);
-        fsm_list = newHead;
+        rec_list = new_head;
         return;
     }
 
   	while((cur) && (cur->next)) {
-  	  if ((*cur->next->fsm).id == id) {
-            fsm_list_node *tmp = cur->next->next;
-            free(cur->next->fsm);
+  	  if ((*cur->next->rec).id == id) {
+            rec_list_node *tmp = cur->next->next;
+            free(cur->next->rec);
             free(cur->next);
 	    cur->next = tmp;
 	    return;
@@ -214,53 +213,53 @@ void __INSTR_fsm_destroy(fsm_id id) {
 }
 
 void __INSTR_check_leaks() {
-    fsm_list_node *cur = fsm_list;
+    rec_list_node *cur = rec_list;
 
     while(cur) {
-        fsm_list_node *tmp = cur->next;
-	if (cur->fsm->state == FSM_STATE_ALLOCATED){
+        rec_list_node *tmp = cur->next;
+	if (cur->rec->state == REC_STATE_ALLOCATED){
 		assert(0 && "memory leak detected");
 		__VERIFIER_error();
 	}
 
-        free(cur->fsm);
+        free(cur->rec);
         free(cur);
         cur = tmp;
     }
 }
 
-void __INSTR_realloc(fsm_id old_id, fsm_id new_id, size_t size) {
+void __INSTR_realloc(rec_id old_id, rec_id new_id, size_t size) {
 	if(new_id == 0){
 	  return; //if realloc returns null, nothing happens
 	}
 	
-	fsm *m = NULL;
+	rec *m = NULL;
 	
 	if(old_id == 0){
-	  m = __INSTR_fsm_create(new_id, FSM_STATE_ALLOCATED);
+	  m = __INSTR_rec_create(new_id, REC_STATE_ALLOCATED);
 	  m->size = size;
 	  return;
 	}
 
-	m = __INSTR_fsm_list_search(old_id);
+	m = __INSTR_rec_list_search(old_id);
 	
 	if (m != NULL) {
-		if(m->state == FSM_STATE_FREED){
+		if(m->state == REC_STATE_FREED){
 		    assert(0 && "realloc on memory that has already been freed");
 		    __VERIFIER_error();
 		}
 		
-		fsm *new_rec = (fsm *) malloc(sizeof(fsm));
+		rec *new_rec = (rec *) malloc(sizeof(rec));
 		new_rec->id = new_id;
 		new_rec->size = size;
-		new_rec->state = FSM_STATE_ALLOCATED;
+		new_rec->state = REC_STATE_ALLOCATED;
 
-		fsm_list_node *node = (fsm_list_node *) malloc(sizeof(fsm_list_node));
+		rec_list_node *node = (rec_list_node *) malloc(sizeof(rec_list_node));
 		node->next = NULL;
-		node->fsm = new_rec;
+		node->rec = new_rec;
 
-		__INSTR_fsm_list_prepend(node);
-		__INSTR_fsm_destroy(old_id);		
+		__INSTR_rec_list_prepend(node);
+		__INSTR_rec_destroy(old_id);		
 	}
 	else{
 		assert(0 && "realloc on not allocated memory");
