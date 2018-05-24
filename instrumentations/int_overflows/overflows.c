2f721d16ed62f3a42d00bfd9b3eb64bfe2bae3cb
Martina Vitovska <409920@mail.muni.cz>
2018-05-24 15:15:16 +0200

    Overflows: checks for short and long

--- instrumentations/int_overflows/overflows.c
+++ instrumentations/int_overflows/overflows.c
@@ -26,7 +26,7 @@ void __INSTR_check_mul(int x, int y) {
 	    assert(0 && "Multiplication: integer overflow!");
     }
     if ((x < INT_MIN / y)) {
-        assert(0 && "Multiplication: integer unerflow!");
+        assert(0 && "Multiplication: integer underflow!");
     }
 
     // 2's complement detection
@@ -43,6 +43,100 @@ int __INSTR_check_div(int op1, int op2) {
   // 2's complement detection
   #if (INT_MIN != -INT_MAX)
     if (op1 == INT_MIN && op2 == -1)  {
+      assert(0 && "Division: integer overflow!");
+    }
+  #endif
+}
+
+void __INSTR_check_add_long(long x, long y) {
+      if((x > 0) && (y > 0) && (x > LONG_MAX - y)) {
+	      assert(0 && "Addition: integer overflow!");
+      }
+
+      if((x < 0) && (y < 0) && (x < LONG_MIN - y)) {
+	      assert(0 && "Addition: integer underflow!");
+      }
+}
+
+void __INSTR_check_sub_long(long x, long y) {
+      if((y > 0) && (x < LONG_MIN +  y)) {
+	      assert(0 && "Substraction: integer underflow!");
+      }
+
+      if((y < 0) && (x > LONG_MAX + y)) {
+	      assert(0 && "Substraction: integer overflow!");
+      }
+}
+
+void __INSTR_check_mul_long(long x, long y) {
+    if (x > LONG_MAX / y) {
+	    assert(0 && "Multiplication: integer overflow!");
+    }
+    if ((x < LONG_MIN / y)) {
+        assert(0 && "Multiplication: integer underflow!");
+    }
+
+    // 2's complement detection
+    #if (LONG_MIN != -LONG_MAX)
+      if ((x == -1) && (y == LONG_MIN))
+      if ((y == -1) && (x == LONG_MIN))
+    #endif
+}
+
+int __INSTR_check_div_long(long op1, long op2) {
+  if (op2 == 0) {
+    assert(0 && "Division by zero!");
+  }
+  // 2's complement detection
+  #if (LONG_MIN != -LONG_MAX)
+    if (op1 == LONG_MIN && op2 == -1)  {
+      assert(0 && "Division: integer overflow!");
+    }
+  #endif
+}
+
+void __INSTR_check_add_short(short x, short y) {
+      if((x > 0) && (y > 0) && (x > SHRT_MAX - y)) {
+	      assert(0 && "Addition: integer overflow!");
+      }
+
+      if((x < 0) && (y < 0) && (x < SHRT_MIN - y)) {
+	      assert(0 && "Addition: integer underflow!");
+      }
+}
+
+void __INSTR_check_sub_short(short x, short y) {
+      if((y > 0) && (x < SHRT_MIN +  y)) {
+	      assert(0 && "Substraction: integer underflow!");
+      }
+
+      if((y < 0) && (x > SHRT_MAX + y)) {
+	      assert(0 && "Substraction: integer overflow!");
+      }
+}
+
+void __INSTR_check_mul_short(short x, short y) {
+    if (x > SHRT_MAX / y) {
+	    assert(0 && "Multiplication: integer overflow!");
+    }
+    if ((x < SHRT_MIN / y)) {
+        assert(0 && "Multiplication: integer underflow!");
+    }
+
+    // 2's complement detection
+    #if (SHRT_MIN != -SHRT_MAX)
+      if ((x == -1) && (y == SHRT_MIN))
+      if ((y == -1) && (x == SHRT_MIN))
+    #endif
+}
+
+int __INSTR_check_div_short(short op1, short op2) {
+  if (op2 == 0) {
+    assert(0 && "Division by zero!");
+  }
+  // 2's complement detection
+  #if (SHRT_MIN != -SHRT_MAX)
+    if (op1 == SHRT_MIN && op2 == -1)  {
       assert(0 && "Division: integer overflow!");
     }
   #endif
