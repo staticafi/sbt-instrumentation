84a09e86a4c75535eab91c58f3b12b83e1713835
Martina Vitovska <409920@mail.muni.cz>
2018-06-12 12:52:11 +0200

    Overflows: do not use math.h

--- instrumentations/int_overflows/overflows.c
+++ instrumentations/int_overflows/overflows.c
@@ -1,23 +1,22 @@
 #include <assert.h>
 #include <stdint.h>
-#include <math.h>
 
 void __INSTR_check_add_i32(int32_t x, int32_t y) {
-      if((x > 0) && (y > 0) && (x > (pow(2, 31) - 1) - y)) {
+      if((x > 0) && (y > 0) && (x > INT32_MAX - y)) {
 	      assert(0 && "Addition: integer overflow!");
       }
 
-      if((x < 0) && (y < 0) && (x < (-pow(2, 31)) - y)) {
+      if((x < 0) && (y < 0) && (x < INT32_MIN - y)) {
 	      assert(0 && "Addition: integer underflow!");
       }
 }
 
 void __INSTR_check_sub_i32(int32_t x, int32_t y) {
-      if((y > 0) && (x < (-pow(2, 31)) +  y)) {
+      if((y > 0) && (x < INT32_MIN +  y)) {
 	      assert(0 && "Substraction: integer underflow!");
       }
 
-      if((y < 0) && (x > (pow(2, 31) - 1) + y)) {
+      if((y < 0) && (x > INT32_MAX + y)) {
 	      assert(0 && "Substraction: integer overflow!");
       }
 }
@@ -34,16 +33,16 @@ void __INSTR_check_mul_i32(int32_t x, int32_t y) {
     if (x == 0 || y == 0)
         return;
 
-    if (x > (pow(2, 31) - 1) / y) {
+    if (x > INT32_MAX / y) {
 	    assert(0 && "Multiplication: integer overflow!");
     }
-    if (x < (-pow(2, 31)) / y) {
+    if (x < INT32_MIN / y) {
         assert(0 && "Multiplication: integer underflow!");
     }
 
-    if ((x == -1) && (y == (-pow(2, 31))))
+    if ((x == -1) && (y == INT32_MIN))
         assert(0 && "Multiplication: integer overflow!");
-    if ((y == -1) && (x == (-pow(2, 31))))
+    if ((y == -1) && (x == INT32_MIN))
         assert(0 && "Multiplication: integer overflow!");
 }
 
@@ -52,27 +51,27 @@ void __INSTR_check_div_i32(int32_t op1, int32_t op2) {
     assert(0 && "Division by zero!");
   }
 
-  if (op1 == (-pow(2, 31)) && op2 == -1)  {
+  if (op1 == INT32_MIN && op2 == -1)  {
     assert(0 && "Division: integer overflow!");
   }
 }
 
 void __INSTR_check_add_i64(int64_t x, int64_t y) {
-      if((x > 0) && (y > 0) && (x > (pow(2, 63) - 1) - y)) {
+      if((x > 0) && (y > 0) && (x > INT64_MAX - y)) {
 	      assert(0 && "Addition: integer overflow!");
       }
 
-      if((x < 0) && (y < 0) && (x < (-pow(2, 63)) - y)) {
+      if((x < 0) && (y < 0) && (x < INT64_MIN - y)) {
 	      assert(0 && "Addition: integer underflow!");
       }
 }
 
 void __INSTR_check_sub_i64(int64_t x, int64_t y) {
-      if((y > 0) && (x < (-pow(2, 63)) +  y)) {
+      if((y > 0) && (x < INT64_MIN +  y)) {
 	      assert(0 && "Substraction: integer underflow!");
       }
 
-      if((y < 0) && (x > (pow(2, 63) - 1) + y)) {
+      if((y < 0) && (x > INT64_MAX + y)) {
 	      assert(0 && "Substraction: integer overflow!");
       }
 }
@@ -81,16 +80,16 @@ void __INSTR_check_mul_i64(int64_t x, int64_t y) {
     if (x == 0 || y == 0)
         return;
 
-    if (x > (pow(2, 63) - 1) / y) {
+    if (x > INT64_MAX / y) {
 	    assert(0 && "Multiplication: integer overflow!");
     }
-    if (x < (-pow(2, 63)) / y) {
+    if (x < INT64_MIN / y) {
         assert(0 && "Multiplication: integer underflow!");
     }
 
-    if ((x == -1) && (y == (-pow(2, 63))))
+    if ((x == -1) && (y == INT64_MIN))
 	  assert(0 && "Multiplication: integer overflow!");
-    if ((y == -1) && (x == (-pow(2, 63))))
+    if ((y == -1) && (x == INT64_MIN))
 	  assert(0 && "Multiplication: integer overflow!");
 }
 
@@ -99,27 +98,27 @@ void __INSTR_check_div_i64(int64_t op1, int64_t op2) {
     assert(0 && "Division by zero!");
   }
 
-  if (op1 == (-pow(2, 63)) && op2 == -1)  {
+  if (op1 == INT64_MIN && op2 == -1)  {
     assert(0 && "Division: integer overflow!");
   }
 }
 
 void __INSTR_check_add_i16(int16_t x, int16_t y) {
-      if((x > 0) && (y > 0) && (x > (pow(2, 15) - 1) - y)) {
+      if((x > 0) && (y > 0) && (x > INT16_MAX - y)) {
 	      assert(0 && "Addition: integer overflow!");
       }
 
-      if((x < 0) && (y < 0) && (x < (-pow(2, 15)) - y)) {
+      if((x < 0) && (y < 0) && (x < INT16_MIN - y)) {
 	      assert(0 && "Addition: integer underflow!");
       }
 }
 
 void __INSTR_check_sub_i16(int16_t x, int16_t y) {
-      if((y > 0) && (x < (-pow(2, 15)) +  y)) {
+      if((y > 0) && (x < INT16_MIN +  y)) {
 	      assert(0 && "Substraction: integer underflow!");
       }
 
-      if((y < 0) && (x > (pow(2, 15) - 1) + y)) {
+      if((y < 0) && (x > INT16_MAX + y)) {
 	      assert(0 && "Substraction: integer overflow!");
       }
 }
@@ -128,16 +127,16 @@ void __INSTR_check_mul_i16(int16_t x, int16_t y) {
     if (x == 0 || y == 0)
         return;
 
-    if (x > (pow(2, 15) - 1) / y) {
+    if (x > INT16_MAX / y) {
 	    assert(0 && "Multiplication: integer overflow!");
     }
-    if (x < (-pow(2, 15)) / y) {
+    if (x < INT16_MIN / y) {
         assert(0 && "Multiplication: integer underflow!");
     }
 
-    if ((x == -1) && (y == (-pow(2, 15))))
+    if ((x == -1) && (y == INT16_MIN))
 	    assert(0 && "Multiplication: integer overflow!");
-    if ((y == -1) && (x == (-pow(2, 15))))
+    if ((y == -1) && (x == INT16_MIN))
 	    assert(0 && "Multiplication: integer overflow!");
 }
 
@@ -146,27 +145,27 @@ void __INSTR_check_div_i16(int16_t op1, int16_t op2) {
     assert(0 && "Division by zero!");
   }
 
-  if (op1 == (-pow(2, 15)) && op2 == -1)  {
+  if (op1 == INT16_MIN && op2 == -1)  {
     assert(0 && "Division: integer overflow!");
   }
 }
 
 void __INSTR_check_add_i8(int8_t x, int8_t y) {
-      if((x > 0) && (y > 0) && (x > (pow(2, 7) - 1) - y)) {
+      if((x > 0) && (y > 0) && (x > INT8_MAX - y)) {
 	      assert(0 && "Addition: int8_t overflow!");
       }
 
-      if((x < 0) && (y < 0) && (x < (-pow(2, 7)) - y)) {
+      if((x < 0) && (y < 0) && (x < INT8_MIN - y)) {
 	      assert(0 && "Addition: int8_t underflow!");
       }
 }
 
 void __INSTR_check_sub_i8(int8_t x, int8_t y) {
-      if((y > 0) && (x < (-pow(2, 7)) +  y)) {
+      if((y > 0) && (x < INT8_MIN +  y)) {
 	      assert(0 && "Substraction: int8_t underflow!");
       }
 
-      if((y < 0) && (x > (pow(2, 7) - 1) + y)) {
+      if((y < 0) && (x > INT8_MAX + y)) {
 	      assert(0 && "Substraction: int8_t overflow!");
       }
 }
@@ -175,16 +174,16 @@ void __INSTR_check_mul_i8(int8_t x, int8_t y) {
     if (x == 0 || y == 0)
         return;
 
-    if (x > (pow(2, 7) - 1) / y) {
+    if (x > INT8_MAX / y) {
 	    assert(0 && "Multiplication: int8_t overflow!");
     }
-    if ((x < (-pow(2, 7)) / y)) {
+    if ((x < INT8_MIN / y)) {
         assert(0 && "Multiplication: int8_t underflow!");
     }
 
-    if ((x == -1) && (y == (-pow(2, 7))))
+    if ((x == -1) && (y == INT8_MIN))
 	    assert(0 && "Multiplication: int8_t overflow!");
-    if ((y == -1) && (x == (-pow(2, 7))))
+    if ((y == -1) && (x == INT8_MIN))
 	    assert(0 && "Multiplication: int8_t overflow!");
 }
 
@@ -193,7 +192,7 @@ void __INSTR_check_div_i8(int8_t op1, int8_t op2) {
     assert(0 && "Division by zero!");
   }
 
-  if (op1 == (-pow(2, 7)) && op2 == -1)  {
+  if (op1 == INT8_MIN && op2 == -1)  {
     assert(0 && "Division: int8_t overflow!");
   }
 }
