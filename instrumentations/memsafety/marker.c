a38b68172720cfe4bd8722c75c67af1aff713536
Marek Chalupa <xchalup4@fi.muni.cz>
2018-06-27 12:08:29 +0200

    marker config: split off mark_free from mark_pointer
    We want to keep these things separate.
    
    Signed-off-by: Marek Chalupa <xchalup4@fi.muni.cz>

--- instrumentations/memsafety/marker.c
+++ instrumentations/memsafety/marker.c
@@ -2,3 +2,8 @@ void __INSTR_mark_pointer(void * __attribute__((unused)) p)
 {
 	// this body will never by used
 }
+
+void __INSTR_mark_free(void * __attribute__((unused)) p)
+{
+	// this body will never by used
+}
