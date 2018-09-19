e5a63da5d20a8ca8ea7663bc356bea6d1d6d359d
Martina Vitovska <409920@mail.muni.cz>
2018-09-19 09:59:29 +0200

    Memsafety refactoring: rename check_bounds_min_lr

--- instrumentations/memsafety/memsafety.c
+++ instrumentations/memsafety/memsafety.c
@@ -323,7 +323,7 @@ void __INSTR_check_pointer(rec_id id, a_size range) {
     }
 }
 
-void __INSTR_check_bounds_min_lr(rec_id addr_a, a_size min_off, a_size min_space, rec_id addr_b, a_size range) {
+void __INSTR_check_bounds_min(rec_id addr_a, a_size min_off, a_size min_space, rec_id addr_b, a_size range) {
     int64_t n = addr_b - addr_a;
     if (-min_off >= n || n + range > min_space) {
         __INSTR_check_pointer(addr_b, range);
