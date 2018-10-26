fb423cf3238c613ceecb9462cbcf2183f777c8e8
Martina Vitovska <409920@mail.muni.cz>
2018-10-26 15:17:50 +0200

    Overflows: add check NSW plugin
    Add plugin that checks whether a binary operation has NSW set.

---
+++ analyses/check_nsw_plugin.cpp
@@ -0,0 +1,43 @@
+#include "check_nsw_plugin.hpp"
+#include <limits>
+#include <cmath>
+
+using namespace llvm;
+
+std::string CheckNSWPlugin::canOverflow(Value* value) {
+    // Support only instructions
+    auto* inst = dyn_cast<Instruction>(value);
+    if (!inst)
+        return "unknown";
+
+    // Support only instruction of integer type
+    const auto* intT = dyn_cast<IntegerType>(inst->getType());
+    if (!intT)
+        return "unknown";
+
+    if (const auto* binOp
+        = dyn_cast<OverflowingBinaryOperator>(inst)) {
+        // we are looking for bin. ops with nsw
+        if (!binOp->hasNoSignedWrap())
+            return "false";
+    }
+
+    return "unknown";
+}
+
+static const std::string supportedQueries[] = {
+    "canOverflow",
+};
+
+bool CheckNSWPlugin::supports(const std::string& query) {
+    for (unsigned idx = 0; idx < sizeof(supportedQueries)/sizeof(*supportedQueries); ++idx) {
+        if (query == supportedQueries[idx])
+            return true;
+    }
+
+    return false;
+}
+
+extern "C" InstrPlugin* create_object(llvm::Module* module) {
+    return new CheckNSWPlugin(module);
+}
---
+++ analyses/check_nsw_plugin.hpp
@@ -0,0 +1,30 @@
+#ifndef RANGE_ANALYSIS_PLUGIN_H
+#define RANGE_ANALYSIS_PLUGIN_H
+
+#include <llvm/IR/Module.h>
+#include <llvm/IR/Value.h>
+#include <llvm/IR/Constants.h>
+#include "instr_plugin.hpp"
+
+class CheckNSWPlugin : public InstrPlugin
+{
+     private:
+        std::string canOverflow(llvm::Value*);
+
+    public:
+        bool supports(const std::string& query) override;
+        std::string query(const std::string& query,
+                const std::vector<llvm::Value *>& operands) {
+            if (query == "canOverflow") {
+                assert(operands.size() == 1 && "Wrong number of operands");
+                return canOverflow(operands[0]);
+            } else {
+                return "unsupported query";
+            }
+        }
+
+        CheckNSWPlugin(llvm::Module* module) : InstrPlugin("CheckNSWPlugin") {}
+};
+
+#endif
+
