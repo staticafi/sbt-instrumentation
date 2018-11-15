2b2e427374b017b070abe753a52902d7aa325d61
Marek Chalupa <xchalup4@fi.muni.cz>
2018-11-15 14:01:12 +0100

    marker config: instrument allocations and frees

--- instrumentations/memsafety/marker.c
+++ instrumentations/memsafety/marker.c
@@ -1,9 +1,18 @@
+// Mark that the pointer may point to invalid memory
 void __INSTR_mark_pointer(void * __attribute__((unused)) p)
 {
 	// this body will never by used
 }
 
+// Mark that the call to free may be invalid or may (not) take
+// part in memory leaking
 void __INSTR_mark_free(void * __attribute__((unused)) p)
+{
+	// this body will never by used
+}
+
+// Mark that this allocation may be leaked
+void __INSTR_mark_allocation(void)
 {
 	// this body will never by used
 }
