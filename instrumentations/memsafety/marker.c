d28bfe4237a5a09e02d35755ea490b82a570efad
Marek Chalupa <xchalup4@fi.muni.cz>
2018-11-19 18:54:30 +0100

    simplify marker config

--- instrumentations/memsafety/marker.c
+++ instrumentations/memsafety/marker.c
@@ -1,12 +1,12 @@
 // Mark that the pointer may point to invalid memory
-void __INSTR_mark_pointer(void * __attribute__((unused)) p)
+void __INSTR_mark_pointer(void)
 {
 	// this body will never by used
 }
 
 // Mark that the call to free may be invalid or may (not) take
 // part in memory leaking
-void __INSTR_mark_free(void * __attribute__((unused)) p)
+void __INSTR_mark_free(void)
 {
 	// this body will never by used
 }
