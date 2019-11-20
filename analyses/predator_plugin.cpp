34dd00a4702a64f08e6dc91bb9a3d9a32de13771
Marek Chalupa <chalupa@fi.muni.cz>
2019-11-20 17:31:32 +0100

    [TEST]: let predator answer only TRUE but no queries

--- analyses/predator_plugin.cpp
+++ analyses/predator_plugin.cpp
@@ -146,11 +146,12 @@ void PredatorPlugin::loadPredatorOutput() {
         llvm::errs() << "PredatorPlugin: Predator found no errors\n";
         abort();
     }
-
 }
 
 bool PredatorPlugin::supports(const std::string& query) {
-    return predatorSuccess && (supportedQueries.find(query) != supportedQueries.end());
+    // FIXME: this is just a test
+    return false;
+    //return predatorSuccess && (supportedQueries.find(query) != supportedQueries.end());
 }
 
 void PredatorPlugin::addReportsForLineErrors(llvm::Module* mod) {
