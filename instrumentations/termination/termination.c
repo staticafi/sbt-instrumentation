9fa18d2dc0b57d5c6679e9850595e2f64703926b
Marek Chalupa <mchqwerty@gmail.com>
2019-11-23 17:43:59 +0100

    Add a new function for checking non-termination
    Is going to be inserted by a different pass.

--- instrumentations/termination/termination.c
+++ instrumentations/termination/termination.c
@@ -2,7 +2,13 @@
 
 extern void __VERIFIER_error() __attribute__((noreturn));
 
-void __INSTR_fail() {
+void __INSTR_fail(void) __attribute__((noreturn));
+void __INSTR_fail(void) {
     assert(0 && "infinite loop");
     __VERIFIER_error();
+}
+
+void __INSTR_check_nontermination(_Bool c) {
+	if (c)
+		__INSTR_fail();
 }
