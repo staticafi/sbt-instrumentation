34bbe496023489aee5943b680ecd2e6fb3562cc3
Marek Chalupa <mchqwerty@gmail.com>
2020-03-16 13:24:59 +0100

    Update null_deref property

--- instrumentations/null_deref/null_deref.c
+++ instrumentations/null_deref/null_deref.c
@@ -1,10 +1,5 @@
-#include <assert.h>
-
-extern void __VERIFIER_error(void) __attribute__((noreturn));
-
-void __INSTR_check_pointer(void* pointer) {
-      if (pointer == 0) {
-	      __VERIFIER_error();
-	      assert(0);
-	}
+void __INSTR_mark_pointer(void)
+{
+	// this body will never by used
 }
+
